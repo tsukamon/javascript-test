f5aee15ea0efa72a763e64fa5f5dd90720d66a46
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-10-29 19:02:32 +0900

    クイズを複数にする、リファクタリング

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
-const answers = [
-  'スーパーファミコン',
-  'プレーステーション２', 
-  'ニンテンドースイッチ', 
-  'ニンテンドーDS'];
-const correct = 'ニンテンドーDS';
+const quiz = [
+  {
+    question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+    answers: [
+      'スーパーファミコン',
+      'プレーステーション２', 
+      'ニンテンドースイッチ', 
+      'ニンテンドーDS'
+    ],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answers: [
+      'MOTHER2',
+      'スーパーマリオブラザーズ３', 
+      'スーパードンキーコング', 
+      '星野カービィ'
+    ],
+    correct: 'MOTHER2'
+  }, {
+    question: 'ファイナルファンタジーⅣの主人公の名前は？',
+    answers: [
+      'フリオニール',
+      'クラウド', 
+      'ティーダ', 
+      'セシル'
+    ],
+    correct: 'セシル'
+  }
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
 
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
 //クイズの問題文、選択を定義
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
-  let buttonLength = $button.length;
   while(buttonIndex < buttonLength){
     //ここに命令
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-  if(correct === e.target.textContent) {
+  if(quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
+    score++;
   } else {
     window.alert('不正解！');
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength) {
+    //問題数があればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がなければこちらを実行
+     window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です！');
   }
 };
 
@@ -34,22 +73,9 @@ const clickHandler = (e) => {
 
 //ボタンをクッリクしたら正誤判定
 let handleIndex = 0;
-const buttonLength = $button.length;
 while (handleIndex < buttonLength) {
   $button[handleIndex].addEventListener('click', (e) => {
     clickHandler(e);
   });
   handleIndex++;
 }
-// $button[0].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-// $button[1].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-// $button[2].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-// $button[3].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
